c4f34bd4c28c706d1d52e54819e93ee58fd955f5
SteveF42 <stevef257@student.vvc.edu>
2021-01-21 21:12:28 -0800

    finished scuffedHoot API

--- routes/api.js
+++ routes/api.js
@@ -15,15 +15,8 @@ router.get('/getQuestion-Queries', async (req, res) => {
 })
 
 //queries a kahoot by id
-router.get('/getQuestion-Queries/:id', async (req, res) =>{
-    try {
-        const curser = await Quiz.find({_id : req.params.id});
-        // const curser = await db.collection('Kahoot').find().toArray();
-        console.log(curser)
-        res.json(curser);
-    } catch (err) {
-        res.status(500).json({ message: err.message });
-    }
+router.get('/getQuestion-Queries/:id', getKahoot, async (req, res) => {
+    res.send(res.kahoot)
 })
 
 //inserts a quiz
@@ -42,6 +35,47 @@ router.post('/sendQuestion-Queries', async (req, res) => {
     }
 })
 
+router.delete('/deleteQuestion-Queries/:id', getKahoot, async (req, res) => {
+    try{
+        await res.kahoot.remove();
+        res.json({message : "removed scuffedHoot"});
+    }catch(err){
+        res.status(500).json({message : error.message});
+    }
+})
 
+router.patch('/updateQuestion-Queries/:id', getKahoot, async(req,res) =>{
+    if(req.body.title != null){
+        res.kahoot.title = req.body.title;
+    }
+    if(req.body.description != null){
+        res.kahoot.description = req.body.description;
+    }
+    if(req.body.questions != null){
+        res.kahoot.questions = req.body.questions;
+    }
+    try{
+        const updatedKahoot = await res.kahoot.save();
+        res.status(201).json(updatedKahoot);
+    }catch(err){
+        res.status(400).json({'message':err.message});
+    }
+})
+
+
+async function getKahoot(req, res, next) {
+    let kahoot;
+    try {
+        kahoot = await Quiz.findById(req.params.id);
+        if (kahoot == null) {
+            return res.status(404).json({ message: 'Not Found' });
+        }
+    } catch (err) {
+        return res.status(405).json({ message: 'invalid ID length' });
+    }
+
+    res.kahoot = kahoot;
+    next();
+}
 
 module.exports = router;
